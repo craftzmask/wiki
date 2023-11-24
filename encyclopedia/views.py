16cd9b86896dd5fa3aac8d5b0ecea309d4de0382
Khanh Chung <khanh.d.chung@gmail.com>
2023-11-23 20:38:35 -0800

    implement view a single entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from . import util
@@ -6,4 +7,14 @@ from . import util
 def index(request):
     return render(request, 'encyclopedia/index.html', {
         'entries': util.list_entries()
+    })
+
+
+def view_entry(request, title):
+    entry = util.get_entry(title)
+    if not entry:
+        raise Http404("Entry does not exist")
+    return render(request, 'encyclopedia/entry.html', {
+        'title': title,
+        'entry': entry
     })
